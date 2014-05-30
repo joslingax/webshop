9fdbc34e18074729e697c2daf9ff227b3c64fc5b
joslingax <joslingax@gmail.com>
2014-05-30 22:38:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 
-var phonecat = angular.module('phonecat',[]);
+//////////////////////////////////////////////////////////////////////////////////////
+//MODULE MIT EINER VARIABLE EINBINDEN
+//////////////////////////////////////////////////////////////////////////////////////
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
+var app = express();//UNSER APP WIRD ERSTELLT
 
-phonecat.controller('CartController',function($scope)
-	{
-		$scope.items= 
-		[
-         {title:'Riccola',quantity:8,price:56.8},
-         {title:'Tomato',quantity:4,price:67.56},
-         {title:'Table',quantity:18,price:6.1},
-         {title:'Chaire',quantity:13,price:8.2},
-         {title:'Cart',quantity:8,price:56.8},
-         {title:'Ball',quantity:8,price:56.8},
-         {title:'Guitar',quantity:8,price:56.8}
-		]
+//////////////////////////////////////////////////////////////////////////////////////
+//CONFIGURATION DER APP
+//////////////////////////////////////////////////////////////////////////////////////
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));//DIE DATEIEN DIE FUER DIE VIEWS ZUSTANDIG SIND,SIND IM ORDNER 'VIEWS'
+    app.set('view engine', 'jade');                 //DAS VIEW ENGINE IST DAS TEMPLATE JADE
+    app.use(express.logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.use(express.methodOverride());
+    app.use(express.static(path.join(__dirname, 'public')));//HIER WIRD DER STANDORT FÜR DIE STASTICHEN DATEIEN FESTGESTELLT
+});
+//////////////////////////////////////////////////////////////////////////////////////
+//MÖGLICHEN ROUTEN DER APP-START
+//////////////////////////////////////////////////////////////////////////////////////
+app.get('/', routes.index);
+app.get('/home', routes.index);
+app.get('/home/data', routes.indexData);
 
-		$scope.remove= function(index)
-		{
-			$scope.items.splice(index,1);
-		}
-	});
+app.get('/showItemDetails', routes.showItemDetails);
+app.get('/showItemDetails/data', routes.showItemDetailsData);
+
+app.post('/search', routes.search);
+
+app.get('/register', routes.register);
+app.post('/registerUser', routes.registerUser);
+
+app.get('/loginPage', routes.loginPage);
+//////////////////////////////////////////////////////////////////////////////////////
+//MÖGLICHEN ROUTEN DER APP-END
+//////////////////////////////////////////////////////////////////////////////////////
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+// JavaScript source code
