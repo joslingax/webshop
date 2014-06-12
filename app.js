e313e892864f039dc1d3ebe4131690bc906058d8
joslingax <joslingax@gmail.com>
2014-06-12 08:49:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-
 //////////////////////////////////////////////////////////////////////////////////////
 //MODULE MIT EINER VARIABLE EINBINDEN
 //////////////////////////////////////////////////////////////////////////////////////
 var express = require('express');
-var routes = require('./routes');
+var routesHome = require('./routes/home');
+var routesRegister = require('./routes/register');
+var routesSearch = require('./routes/search');
+var routesSelectedItem = require('./routes/showSelectedItem');
+var routesSignIn = require('./routes/SignIn');
+var routesToCart= require('./routes/toCart');
+var cart = require('./core/cart');
+
 var http = require('http');
 var path = require('path');
-
 var app = express();//UNSER APP WIRD ERSTELLT
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -18,29 +23,44 @@ app.configure(function () {
     app.set('view engine', 'jade');                 //DAS VIEW ENGINE IST DAS TEMPLATE JADE
     app.use(express.logger('dev'));
     app.use(express.json());
+    app.use(express.favicon());
     app.use(express.urlencoded());
+    app.use(express.cookieParser());//DAMIT IST ES MÖGLICH,DIE COOKIE VARIABLEN ZU BENUTZEN
+    app.use(express.session({secret:'this is a secret'}));//DAMIT IST ES MÖGLICH,DIE SESSION VARIABLEN ZU BENUTZEN
     app.use(express.methodOverride());
-    app.use(express.static(path.join(__dirname, 'public')));//HIER WIRD DER STANDORT FÜR DIE STASTICHEN DATEIEN FESTGESTELLT
+    app.use(express.static(path.join(__dirname, 'public')));//HIER WIRD DER STANDORT FUER DIE STASTICHEN DATEIEN FESTGESTELLT
 });
-//////////////////////////////////////////////////////////////////////////////////////
-//MÖGLICHEN ROUTEN DER APP-START
-//////////////////////////////////////////////////////////////////////////////////////
-app.get('/', routes.index);
-app.get('/home', routes.index);
-app.get('/home/data', routes.indexData);
 
-app.get('/showItemDetails', routes.showItemDetails);
-app.get('/showItemDetails/data', routes.showItemDetailsData);
-
-app.post('/search', routes.search);
-
-app.get('/register', routes.register);
-app.post('/registerUser', routes.registerUser);
-
-app.get('/loginPage', routes.loginPage);
+cart.createCart(app);
 //////////////////////////////////////////////////////////////////////////////////////
-//MÖGLICHEN ROUTEN DER APP-END
+//MOEGLICHEN ROUTEN DER APP-START
 //////////////////////////////////////////////////////////////////////////////////////
+app.get('/', routesHome.index);
+app.get('/home', routesHome.index);
+app.get('/home/data', routesHome.indexData);
+app.post('/home/query',routesHome.indexDataFilter);
+
+
+app.get('/showItemDetails', routesSelectedItem.showItemDetails);
+app.get('/showItemDetails/data', routesSelectedItem.showItemDetailsData);
+
+app.post('/search', routesSearch.search);
+
+app.get('/register', routesRegister.register);
+app.post('/registerUser', routesRegister.registerUser);
+app.get('/registerConfirm', routesRegister.registerConfirm);
+
+app.get('/loginPage', routesSignIn.loginPage);
+app.get('/addToCart', routesToCart.cart);
+app.get('/addToCart/data', routesToCart.cartData);
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+//MoeGLICHEN ROUTEN DER APP-END
+//////////////////////////////////////////////////////////////////////////////////////
+
+app.use(app.router);// AppRouter soll am Ende des Codes sein vor der Listen...
+                             //anweisung,sonst wird die Session objekten nicht erkannt
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
